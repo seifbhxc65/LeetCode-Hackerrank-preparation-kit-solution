8213b013f48db577a7bc4ae6de079c8482b5ff3a
seif belhaj <38863465+seifbhxc65@users.noreply.github.com>
2022-07-12 20:46:11 +0200

    writing solution

--- time_coversion.java
+++ time_coversion.java
@@ -1,1 +1,59 @@
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+    String format="";
+    if(s.indexOf("PM")!=-1) format="PM";
+    else
+    format="AM";
+    String hour=s.substring(0, 2);
+    if(hour.indexOf("12")==-1)
+   { 
+        if(format=="PM" )  return  String.valueOf(Integer.valueOf(hour)+12)+s.substring(2, 8) ;
+    }
+    else{
+        if(format=="AM") return "00"+s.substring(2, 8) ;
+    }
+    return s.substring(0, 8);
+      
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+    
